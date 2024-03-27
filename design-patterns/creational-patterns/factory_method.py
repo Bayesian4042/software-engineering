8dc5903ffe35116e8e5c5e7f0673bb0e05d7c7ee
Abhilasha <singhabhilasha4042@gmail.com>
2024-03-27 20:55:28 +0530

    adds factory method pattern benefits

---
+++ design-patterns/creational-patterns/factory_method.py
@@ -0,0 +1,15 @@
+"""
+Define an interface for creating an object, but let subclasses decide which class to instantiate. 
+Factory Method lets a class defer instantiation to subclasses.
+
+When to use:
+When a caller can't anticipate the types of objects it must create.
+When you have many objects of common type. 
+
+Two variants:
+1. Parameterized Factory Method
+2. Classic GoF
+"""
+
+
+
