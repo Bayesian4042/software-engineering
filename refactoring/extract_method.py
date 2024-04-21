957ee2f45e05ec278f8a4081b03fbf56950bb07d
Abhilasha <singhabhilasha4042@gmail.com>
2024-04-21 21:40:38 +0530

    adds extract method refactoring

---
+++ refactoring/extract_method.py
@@ -0,0 +1,13 @@
+
+# Problem
+
+def print_owing():
+    print_banner()
+
+    # Print details.
+    print("name:", name)
+    print("amount:", get_outstanding())
+
+# Issue
+## The more lines found in a method, the harder it’s to figure out what the method does. This is the main reason for this refactoring.
+
